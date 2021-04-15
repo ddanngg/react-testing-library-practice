07a40914b1ef220e991d33deb7da5815c7f67b38
Nguyen An <an.nguyen@spiritlabs.co>
2021-04-15 14:02:39 +0700

    add: alternative improve test readable

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import {render, fireEvent} from '@testing-library/react'
+import {render /* fireEvent */} from '@testing-library/react'
+import user from '@testing-library/user-event'
 
 import {FavoriteNumber} from '../favorite-number'
 
@@ -8,6 +9,7 @@ test('entering an invalid input value', () => {
   const screen = render(<FavoriteNumber />)
   const input = screen.getByLabelText(/favorite number/i)
 
-  fireEvent.change(input, {target: {value: '10'}})
+  user.type(input, '10')
+  // fireEvent.change(input, {target: {value: '10'}})
   expect(screen.getByRole('alert')).toHaveTextContent(/number is invalid/i)
 })
