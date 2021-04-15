a53f16bc85acd8cbe7ad9decef1d42f39809bc81
Nguyen An <an.nguyen@spiritlabs.co>
2021-04-15 12:38:33 +0700

    add: testing label focus to input with attr-for

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render, screen} from '@testing-library/react'
 
 import {FavoriteNumber} from '../favorite-number'
 
 test("render a number input with label 'Favorite Number'", () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  render(<FavoriteNumber />)
+  const input = screen.getByLabelText(/favorite number/i)
+  expect(input).toHaveAttribute('type', 'number')
 })
