d2865b680d4787d3fef5d0d6e3619758acd11b18
Nguyen An <an.nguyen@spiritlabs.co>
2021-04-16 09:59:52 +0700

    add: clearMock after test

--- src/__tests__/error-boundary.js
+++ src/__tests__/error-boundary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {render} from '@testing-library/react'
+import user from '@testing-library/user-event'
 
 import {ErrorBoundary} from '../error-boundary'
 import {reportError as mockReportError} from '../api'
@@ -28,7 +29,6 @@ function Bomb({shouldThrow}) {
 
 test('calls report error and render there was a problem', () => {
   mockReportError.mockResolvedValueOnce({success: true})
-
   const screen = render(
     <ErrorBoundary>
       <Bomb />
@@ -40,11 +40,26 @@ test('calls report error and render there was a problem', () => {
       <Bomb shouldThrow={true} />
     </ErrorBoundary>,
   )
-
   const error = expect.any(Error)
   const info = {componentStack: expect.stringContaining('Bomb')}
   expect(mockReportError).toHaveBeenCalledWith(error, info)
   expect(mockReportError).toHaveBeenCalledTimes(1)
-
   expect(console.error).toBeCalledTimes(2) // 1 jest-dom, 1 react-dom
+  expect(screen.getByRole('alert')).toBeInTheDocument()
+
+  mockReportError.mockClear()
+  console.error.mockClear()
+
+  screen.rerender(
+    <ErrorBoundary>
+      <Bomb />
+    </ErrorBoundary>,
+  )
+  const tryAgainBtn = screen.getByText(/try again/i)
+  user.click(tryAgainBtn)
+
+  expect(mockReportError).not.toHaveBeenCalled()
+  expect(console.error).not.toHaveBeenCalled()
+  expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+  expect(tryAgainBtn).not.toBeInTheDocument()
 })
